6e0be197611f6f260298377f24535f9627e9705d
Hafeez Hamdan <s1462318@bioinfmsc8.bio.ed.ac.uk>
2022-10-28 12:27:37 +0100

    Made the second shell to automatically run and execute the subsequent shell file, samtools.sh

--- samtools.sh
+++ samtools.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-for j in $(ls /home/$USER/ICA1/Mapping/*.sam); 
+for k in $(ls /home/$USER/ICA1/Mapping/*.sam); 
 do
 	samtools sort -o ${j}Sorted.bam ${j};
 done
 ls
 
+for k in *.bam;
+do
+	basename $k .bam;
+done | sed ':a;N;$!ba;s/\n/ /g' # Get rid of the annoying .samSorted.bam file names
+
 ls *.bam | parallel samtools index '{}'
 
+for l in *.bai;
+do
+        basename $l .bai;
+done | sed ':a;N;$!ba;s/\n/ /g' # Get rid of the annoying .samSorted.bam.bai file names
 
-
-source bedtools.sh
+source /home/$USER/bedtools.sh

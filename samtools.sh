e26d438a7aea1cb7b63b93406d8d3f7d6e870a30
Hafeez Hamdan <s1462318@bioinfmsc8.bio.ed.ac.uk>
2022-10-28 12:32:12 +0100

    Got rid of the annoying .samSorted.bam.bai after sorting & indexing our files with Samtools

--- samtools.sh
+++ samtools.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+# This should sort our sam files accordinly before they can all be used to map against our bedfile with bedtools
 for k in $(ls /home/$USER/ICA1/Mapping/*.sam); 
 do
 	samtools sort -o ${j}Sorted.bam ${j};
 done
-ls
 
+# Get rid of the annoying .samSorted.bam file names
 for k in *.bam;
 do
 	basename $k .bam;
-done | sed ':a;N;$!ba;s/\n/ /g' # Get rid of the annoying .samSorted.bam file names
+done | sed ':a;N;$!ba;s/\n/ /g'
+ls
 
+# This should index all our bam files
 ls *.bam | parallel samtools index '{}'
 
+# Get rid of the annoying .samSorted.bam.bai file names
 for l in *.bai;
 do
         basename $l .bai;
-done | sed ':a;N;$!ba;s/\n/ /g' # Get rid of the annoying .samSorted.bam.bai file names
+done | sed ':a;N;$!ba;s/\n/ /g'
+ls
+
 
 source /home/$USER/bedtools.sh

4f105426d21fe09444d10abe9580311281d18299
BB215066-2022 <s1462318@sms.ed.ac.uk>
2022-11-19 15:33:15 +0000

    Made the script more interactive and added blastp function

--- main.py
+++ main.py
@@ -9,28 +9,51 @@ import os, sys, subprocess, shutil
 os.mkdir("ICA2")
 
 # Search against the NCBI database for all Trehalose-6-Phosphate Synthase protein entries across all mosquitoes species under the taxonomic group Culicidae (TaxonID 7157)
-subprocess.call("esearch -db protein -query \"txid7157[Organism:exp] AND Trehalose-6-Phosphate Synthase\" ")
+subprocess.call("esearch -db protein -query \"txid7157[Organism:exp] AND Trehalose-6-Phosphate Synthase\"",shell=True)
 
-
-# Search against the NCBI database for all Trehalose-6-Phosphate Synthase UID# across all mosquitoes species under the taxonomic group Culicidae (TaxonID 7157)
-subprocess.call("esearch -db protein -query \"txid7157[Organism:exp] AND Trehalose-6-Phosphate Synthase\" | efetch -format uid > ICA2/T6PS.gis",shell=True)
-TempFileA = open("ICA2/T6PS.gis").read().upper()
+# Search against the NCBI database for all Trehalose-6-Phosphate Synthase UID# across all mosquitoes species under the taxonomic group Culicidae (TaxonID 7157) and fetch their UID#)
+subprocess.call("esearch -db protein -query \"txid7157[Organism:exp] AND Trehalose-6-Phosphate Synthase\" | efetch -format uid > ICA2/T6PSynthase.gis",shell=True)
+TempFileA = open("ICA2/T6PSynthase.gis").read().upper()
 len(TempFileA)
-# head -5 ICA2/T6PS.gis
-# wc -l ICA2/T6PS.gis
+print(TempFileA)
 
-
-# Search against the NCBI database for all Trehalose-6-Phosphate Synthase Accession# across all mosquitoes species under the taxonomic group Culicidae (TaxonID 7157)
-subprocess.call("esearch -db protein -query \"txid7157[Organism:exp] AND Trehalose-6-Phosphate Synthase\" | efetch -format acc > ICA2/T6PS.acc",shell=True)
-TempFileB = open("ICA2/T6PS.acc").read().upper()
+# Search against the NCBI database for all Trehalose-6-Phosphate Synthase Accession# across all mosquitoes species under the taxonomic group Culicidae (TaxonID 7157 and fetch their Accession#)
+subprocess.call("esearch -db protein -query \"txid7157[Organism:exp] AND Trehalose-6-Phosphate Synthase\" | efetch -format acc > ICA2/T6PSynthase.acc",shell=True)
+TempFileB = open("ICA2/T6PSynthase.acc").read().upper()
 len(TempFileB)
-# head -5 ICA2/T6PS.acc
-# wc -l ICA2/T6PS.acc
-
+print(TempFileB)
 
 # Search against the NCBI database for all Trehalose-6-Phosphate Synthase protein entries across all mosquito species under the taxonomic group Culicidae (TaxonID 7157) and fetch their fasta sequences
-subprocess.call("esearch -db protein -query \"txid7157[Organism:exp] AND Trehalose-6-Phosphate Synthase\" | efetch -format fasta > ICA2/T6PS.fasta3",shell=True)
-TempFileC = open("ICA2/T6PS.fasta3").read().upper()
+subprocess.call("esearch -db protein -query \"txid7157[Organism:exp] AND Trehalose-6-Phosphate Synthase\" | efetch -format fasta > ICA2/T6PSynthase.fasta",shell=True)
+TempFileC = open("ICA2/T6PSynthase.fasta").read().upper()
 len(TempFileC)
-# head -5 ICA2/T6PS.fasta3
-# grep -c ">" ICA2/T6PS.fasta3
+print(TempFileC)
+
+# Fetch the protein sequence in fasta based on the user-input Accession#
+Seq = input('Select the accession number for the protein sequence to align against the rest of the sequences: ')
+print(f"You selected the protein sequence {Seq}")
+subprocess.call("esearch -db protein -query \"Seq[Accession]\" | efetch -format fasta > ICA2/Seq.fasta",shell=True)
+TempFileD = open("ICA2/EAA12459.4.fasta").read().upper()
+len(TempFileD)
+print(TempFileD)
+
+subprocess.call("makeblastdb -in \"ICA2/T6PSynthase.fasta\" -title \"T6PSynthase\" -dbtype prot -out ICA2/T6PSynthase",shell=True)
+subprocess.call("blastp -query \"ICA2/EAA12459.4.fasta\" -db \"ICA2/T6PSynthase\" -out \"ICA2/T6PSynthaseAlignment.txt\" -outfmt 7",shell=True)
+TempFileE = open("ICA2/T6PSynthaseAlignment.txt").read().upper()
+print(TempFileE)
+
+
+# OptionA = ['yes', 'y']
+# options = ['no', 'n']
+#
+# while True:
+#    user_input = input('Would you like to select a sequence to run an alignment for? Answer yes or no: ')
+#    if user_input.lower() in OptionA:
+#        print('user typed yes')
+#        break
+#    elif user_input.lower() in OptionB:
+#        print('user typed no')
+#        break
+#    else:
+#        print('Type yes or no')
+#        continue

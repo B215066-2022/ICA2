5d76ebd9414a55fcb5bb1ec4acab1e7e29f26c39
BB215066-2022 <s1462318@sms.ed.ac.uk>
2022-11-20 09:43:39 +0000

    Made the script to be more interactive by asking for user input protein sequences

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-import os, sys, subprocess, shutil
+import os
+import sys
+import shutil
+import subprocess # We will be running a lot of background functions that are non python-based
+import time # We need this module to gap each print statement
 
 # export NCBI_API_KEY=54b38a82050346352dbaf27e8e02a65f0408" >> ~/.bashrc
 # export NCBI_API_KEY=54b38a82050346352dbaf27e8e02a65f0408" >> ~/.bash_profile
@@ -9,64 +13,77 @@ import os, sys, subprocess, shutil
 # Make a working directory first, and let's call it ICA2
 os.mkdir("ICA2")
 
-# Search against the NCBI database for all Trehalose-6-Phosphate Synthase protein sequences across all mosquitoes species under the taxonomic group Culicidae (TaxonID 7157)
-subprocess.call("esearch -db protein -query \"txid7157[Organism:exp] AND Trehalose-6-Phosphate Synthase\"",shell=True)
+Option1 = 
+print("This programme will help you analyse protein sequences of your choice.")
+time.sleep(1)
+print("First, I need you to specify the name of the organism where your protein may be found e.g mosquito.")
+time.sleep(1)
+print("If you know the organism taxonomy ID, you can write it in e.g TXID7157.")
+time.sleep(1)
+QueryA = input('Please input your protein family now: ').upper()
+print("Now, I need you to specify the family of protein you are interested in e.g glucose-6-phosphate dehydrogenase.")
+time.sleep(1)
+QueryB = input('Please input your protein family name now: ').upper()
 
-# Search against the NCBI database for all Trehalose-6-Phosphate Synthase protein sequences across all mosquitoes species under the taxonomic group Culicidae (TaxonID 7157) and fetch their UID#
-subprocess.call("esearch -db protein -query \"txid7157[Organism:exp] AND Trehalose-6-Phosphate Synthase\" | efetch -format uid > ICA2/T6PSynthase.gis",shell=True)
-TempFileA = open("ICA2/T6PSynthase.gis").read().upper()
-len(TempFileA)
-print(TempFileA)
+OptionA = ['yes','y']
+OptionB = ['no','n']
+while True:        
+        print(f"You have specified the protein family, {QueryB} in {QueryA}. Is this correct?")
+        QueryX = input("Answer yes or no: ")
 
-# Search against the NCBI database for all Trehalose-6-Phosphate Synthase protein sequences across all mosquito species under the taxonomic group Culicidae (TaxonID 7157) and fetch their Accession numbers
-subprocess.call("esearch -db protein -query \"txid7157[Organism:exp] AND Trehalose-6-Phosphate Synthase\" | efetch -format acc > ICA2/T6PSynthase.acc",shell=True)
-TempFileB = open("ICA2/T6PSynthase.acc").read().upper()
-len(TempFileB)
-print(TempFileB)
+        if QueryX.lower() in OptionA:
+            print(f"We will begin searching NCBI database for {QueryB} in {QueryA}.")
+            # Search against the NCBI database for all Trehalose-6-Phosphate Synthase protein sequences across all mosquitoes species under the taxonomic group Culicidae (TaxonID 7157)
+            print(f"esearch -db protein -query \"{QueryA}[Organism:exp] AND {QueryB}.")
+            subprocess.call(f"esearch -db protein -query \"{QueryA}[Organism:exp] AND {QueryB}\"",shell=True)
+            break # Must have a way of parsing an error message if no sequence count is found
+        if QueryX.lower() in OptionB:
+            print("Too bad...")
+            break
+        else:
+            print("Answer yes or no only.")
+            continue
 
-# Search against the NCBI database for all Trehalose-6-Phosphate Synthase protein sequences across all mosquito species under the taxonomic group Culicidae (TaxonID 7157) and fetch their Fasta sequences
-subprocess.call("esearch -db protein -query \"txid7157[Organism:exp] AND Trehalose-6-Phosphate Synthase\" | efetch -format fasta > ICA2/T6PSynthase.fasta",shell=True)
-TempFileC = open("ICA2/T6PSynthase.fasta").read().upper()
-len(TempFileC)
-print(TempFileC)
+            # Search against the NCBI database for all Trehalose-6-Phosphate Synthase protein sequences across all mosquitoes species under the taxonomic group Culicidae (TaxonID 7157) and fetch their UID#
+            # subprocess.call(f"esearch -db protein -query \"{QueryA}[Organism:exp] AND {QueryB}\" | efetch -format uid > ICA2/{QueryB}.gis",shell=True)
+            # TempFileA = open(f"ICA2/{QueryB}.gis").read().upper()
+            # len(TempFileA)
+            # print(TempFileA)
 
-# Fetch the protein sequence in fasta based on the user-input Accession numbers
-Seq = input('Select the accession number for the protein sequence to align against the rest of the sequences: ')
-print(f"You selected the protein sequence {Seq}")
-subprocess.call(f"esearch -db protein -query \"{Seq}[Accession]\" | efetch -format fasta > ICA2/{Seq}.fasta",shell=True)
-TempFileD = open(f"ICA2/{Seq}.fasta").read().upper()
-len(TempFileD)
-print(TempFileD)
+            # Search against the NCBI database for all Trehalose-6-Phosphate Synthase protein sequences across all mosquito species under the taxonomic group Culicidae (TaxonID 7157) and fetch their Accession numbers
+            subprocess.call(f"esearch -db protein -query \"{QueryA}[Organism:exp] AND {QueryB}\" | efetch -format acc > ICA2/{QueryB}{QueryA}.acc",shell=True)
+            TempFileB = open("ICA2/{QueryB}{QueryA}.acc").read().upper()
+            len(TempFileB)
+            print(TempFileB)
 
-# Create a Blast database for the protein sequences we will align later
-# Indexing our database will make our alignment work so much faster
-subprocess.call("makeblastdb -in \"ICA2/T6PSynthase.fasta\" -title \"T6PSynthase\" -dbtype prot -out ICA2/T6PSynthase",shell=True)
+            # Search against the NCBI database for all Trehalose-6-Phosphate Synthase protein sequences across all mosquito species under the taxonomic group Culicidae (TaxonID 7157) and fetch their Fasta sequences
+            subprocess.call(f"esearch -db protein -query \"{QueryA}[Organism:exp] AND {QueryB}\" | efetch -format fasta > ICA2/{QueryB}{QueryA}.fasta",shell=True)
+            TempFileC = open("ICA2/{QueryB}{QueryA}.fasta").read().upper()
+            len(TempFileC)
+            print(TempFileC)
 
-# Align the user-input protein sequence with all the other protein sequences using Blastp
-# Print the blast alignment output in text
-subprocess.call(f"blastp -query \"ICA2/{Seq}.fasta\" -db \"ICA2/T6PSynthase\" -out \"ICA2/T6PSynthaseBAlignment.txt\" -outfmt 7",shell=True)
-TempFileE = open("ICA2/T6PSynthaseBAlignment.txt").read().upper()
-print(TempFileE)
+            # Fetch the protein sequence in fasta based on the user-input Accession numbers
+            QueryS = input('Select the accession number for the protein sequence to align against the rest of the sequences: ')
+            print(f"You selected the protein sequence {QueryS}")
+            subprocess.call(f"esearch -db protein -query \"{QueryS}[Accession]\" | efetch -format fasta > ICA2/{QueryS}.fasta",shell=True)
+            TempFileD = open(f"ICA2/{QueryS}.fasta").read().upper()
+            len(TempFileD)
+            print(f"Showing you the protein sequence for the protein sequence {QueryS}...")
+            print(TempFileD)
 
-# Align the user-input protein sequence with all the other protein sequences using Clustalo
-# Print the Clustalo alignment output in fasta
-subprocess.call("clustalo -i \"ICA2/T6PSynthase.fasta\" -o \"ICA2/T6PSynthaseCAlignment.fasta\" --auto -v",shell=True)
-TempFileF = open("ICA2/T6PSynthaseCAlignment.fasta").read().upper()
-print(TempFileF)
+            # Create a Blast database by indexing all the protein sequences
+            # Indexing our database will make our alignment work so much faster
+            subprocess.call("makeblastdb -in \"ICA2/{QueryS}.fasta\" -title \"{QueryS}\" -dbtype prot -out ICA2/{QueryS}",shell=True)
 
+            # Align the user-input protein sequence with all the other protein sequences using Blastp
+            # Print the blast alignment output in text
+            subprocess.call(f"blastp -query \"ICA2/{QueryS}.fasta\" -db \"ICA2/{QueryS}\" -out \"ICA2/{QueryS}Blasted.txt\" -outfmt 7",shell=True)
+            TempFileE = open(f"ICA2/{QueryS}Blasted.txt").read().upper()
+            print(TempFileE)
 
+            # Align the user-input protein sequence with all the other protein sequences using Clustalo
+            # Print the Clustalo alignment output in fasta
+            subprocess.call(f"clustalo -i \"ICA2/{QueryS}.fasta\" -o \"ICA2/{QueryS}Alignment.fasta\" --auto -v",shell=True)
+            TempFileF = open(f"ICA2/{QueryS}Alignment.fasta").read().upper()
+            print(TempFileF)
 
-# OptionA = ['yes', 'y']
-# options = ['no', 'n']
-#
-# while True:
-#    user_input = input('Would you like to select a sequence to run an alignment for? Answer yes or no: ')
-#    if user_input.lower() in OptionA:
-#        print('user typed yes')
-#        break
-#    elif user_input.lower() in OptionB:
-#        print('user typed no')
-#        break
-#    else:
-#        print('Type yes or no')
-#        continue

d49e110bfac8668b355ebf65026905ffc3aeecf6
BB215066-2022 <s1462318@sms.ed.ac.uk>
2022-11-20 18:24:22 +0000

    Got ClustalO alignment to work perfectly!

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ while True:
             #        print("...")
 
             print(f"------------------------------")
-            print(f"We will now begin fetching all the entries in the form of UID for {QueryB} in the organism under the TaxonID {QueryA}")
+            print(f"We will now begin fetching all the entries in the form of UID for {QueryB} in the organism under the TaxonID {QueryA}.")
             time.sleep(3)
             # Search against the NCBI database for the user-input protein family across all species under the specified taxon group and fetch their UID#
             subprocess.call(f"esearch -db protein -query \"txid{QueryA}[Organism:exp] AND {QueryB}\" | efetch -format uid > \"ICA2/{QueryBr}.gis\" ",shell=True)
@@ -77,12 +77,12 @@ while True:
             # Must have a way of parsing an error message if no sequence count is found using re.findall
 
             print(f"------------------------------")
-            print(f"We will now begin fetching all the protein entries in the form of accession numbers for {QueryB} in the organism under the TaxonID {QueryA}")
+            print(f"We will now begin fetching all the protein entries in the form of accession numbers for {QueryB} in the organism under the TaxonID {QueryA}.")
             time.sleep(3)   
             # Search against the NCBI database for the user-input protein family across all species under the specified taxon group and fetch their Accession numbers
             subprocess.call(f"esearch -db protein -query \"txid{QueryA}[Organism:exp] AND {QueryB}\" | efetch -format acc > \"ICA2/{QueryBr}.acc\" ",shell=True)
             TempFileB = open(f"ICA2/{QueryBr}.acc").read().upper()
-            print(f"I will now list down all the accession numbers for {QueryB} in the organism under the TaxonID {QueryA}")
+            print(f"I will now list down all the accession numbers for {QueryB} in the organism under the TaxonID {QueryA}.")
             print(TempFileB)
 
             
@@ -95,7 +95,7 @@ while True:
             TempFileC = open(f"ICA2/{QueryBr}.fasta").read().upper()
             print(f"Would you like me to list down all the FASTA sequences? Just a warning, it can be overwhelming if you choose to: ")
             time.sleep(1)
-            QueryC = input("Answer yes or no:")
+            QueryC = input("Answer yes or no: ")
             
             while True:
                 if QueryC.lower() in OptionA:
@@ -112,6 +112,7 @@ while True:
                     continue
 
             # Fetch the protein sequence in fasta based on the user-input Accession number
+            print(f"------------------------------")
             print("Now, I need to know which protein sequence are you interested in analysing.") 
             time.sleep(1)
             QueryD = input('Input the accession number for the protein sequence of your choice: ')
@@ -123,7 +124,7 @@ while True:
             
             print(f"Would you like me to show you the protein sequence {QueryD} you selected?")
             time.sleep(1)
-            QueryE = input("Answer yes or no:")
+            QueryE = input("Answer yes or no: ")
 
             while True:
                 if QueryE.lower() in OptionA:
@@ -138,20 +139,94 @@ while True:
                 else:
                     print("Answer yes or no only.")
                     continue
-
-            # Create a Blast database by indexing all the protein sequences
+            
+            print(f"------------------------------")
+            print("Great, we have all the data we need for our protein analysis!")
+            time.sleep(1)
+            print("But before going any further, we will need to create a database and index our protein sequences.")
+            time.sleep(1)
+            print(f"Indexing our {QueryB} protein sequences in the organisms under the TaxonID {QueryA}...")
+            time.sleep(2)
+            print(f"Generating database...")
+            time.sleep(2)
+            # Create a Blast database by indexing all the protein sequences in the Fasta file we initially fetched
             # Indexing our database will make our alignment work so much faster
-            # subprocess.call("makeblastdb -in \"ICA2/{QueryS}.fasta\" -title \"{QueryS}\" -dbtype prot -out \"ICA2/{QueryS}\" ",shell=True)
-            # Add a print statement here stating that indexing database is needed
+            subprocess.call(f"makeblastdb -in \"ICA2/{QueryBr}.fasta\" -title \"{QueryBr}\" -dbtype prot -out \"ICA2/{QueryBr}\" ", shell=True) # {QueryBr}.fasta because this file contained the fasta sequence for all protein entries based on the user-input {QueryB}
+            print("Database created!")
+            time.sleep(1)
+
             # Align the user-input protein sequence with all the other protein sequences using Blastp
             # Print the blast alignment output in text
-            # subprocess.call(f"blastp -query \"ICA2/{QueryS}.fasta\" -db \"ICA2/{QueryS}\" -out \"ICA2/{QueryS}Blasted.txt\" -outfmt 7",shell=True)
-            # TempFileE = open(f"ICA2/{QueryS}Blasted.txt").read().upper()
-            # print(TempFileE)
+            print(f"Now, we will begin aligning {QueryD} against the other protein sequences with Blastp.")
+            time.sleep(1)
+            print(f"Running Blast alignment between {QueryD} and all the other {QueryB} protein sequences...")
+            time.sleep(3)
+            subprocess.call(f"blastp -query \"ICA2/{QueryD}.fasta\" -db \"ICA2/{QueryBr}\" -out \"ICA2/{QueryD}Blasted.txt\" -outfmt 7",shell=True)
+            TempFileE = open(f"ICA2/{QueryD}Blasted.txt").read().upper()
+            
+            print(f"Would you like to view the result of our Blast alignment?")
+            time.sleep(1)
+            QueryF = input("Answer yes or no: ")
 
-            # Align the user-input protein sequence with all the other protein sequences using Clustalo
-            # Print the Clustalo alignment output in fasta
-            # subprocess.call(f"clustalo -i \"ICA2/{QueryS}.fasta\" -o \"ICA2/{QueryS}Alignment.fasta\" --auto -v",shell=True)
+            while True:
+                if QueryF.lower() in OptionA:
+                    time.sleep(1)
+                    print(f"This is the Blast alignment output for {QueryD}: ")
+                    time.sleep(1)
+                    print(TempFileE)
+                    time.sleep(3)
+                    print(f"I have saved this Blast alignment output in <ICA2/{QueryD}Blasted.txt>")
+                    break
+                elif QueryF.lower() in OptionB:
+                    print("We can continue next time.")
+                    break
+                else:
+                    print("Answer yes or no only.")
+                    continue
+
+            print(f"Would you like to run mutiple sequence alignment with ClustalO now?")
+            time.sleep(1)
+            QueryG = input("Answer yes or no: ")
+
+            while True:
+                if QueryG.lower() in OptionA:
+                    time.sleep(1)
+                # Align the user-input protein sequence with all the other protein sequences using Clustalo
+                # Print the Clustalo alignment output in fasta
+                    print(f"Now, we will begin aligning {QueryD} against the other protein sequences with ClustalO.")
+                    time.sleep(1)
+                    print(f"Running ClustalO alignment between {QueryD} and all the other {QueryB} protein sequences...")
+                    time.sleep(3)
+                    subprocess.call(f"clustalo -i \"ICA2/{QueryBr}.fasta\" -o \"ICA2/{QueryBr}Aligned.fasta\" --auto -v",shell=True)
+                    TempFileF = open(f"ICA2/{QueryBr}Aligned.fasta").read().upper()
+            
+                    print(f"Would you like to view the result of our ClustalO alignment?")
+                    time.sleep(1)
+                    QueryH = input("Answer yes or no: ")
+
+                    while True:
+                        if QueryH.lower() in OptionA:
+                            time.sleep(1)
+                            print(f"This is the ClustalO alignment output for {QueryBr}: ")
+                            time.sleep(1)
+                            print(TempFileF)
+                            time.sleep(3)
+                            print(f"I have saved this ClustalO alignment output in <ICA2/{QueryD}Aligned.fasta>")
+                            break
+                        elif QueryH.lower() in OptionB:
+                            print("We can view the result of ClustalO alignment next time.")
+                            break
+                        else:
+                            print("Answer yes or no only.")
+                            continue    
+                    break
+                elif QueryG.lower() in OptionB:
+                    print("We can run multiple sequence alignment with Clustalo next time.")
+                    break
+                else:
+                    print("Answer yes or no only.")
+                    continue
+
             break
 
         elif QueryX.lower() in OptionB:
